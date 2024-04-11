ed88fe9555b33a55513c267285753c852e6365e5
viniciusdev772 <viniciusdev37@gmail.com>
2024-04-11 20:41:08 -0300

    Atualizar middleware de sobrecarga para bloquear e desbloquear IPs em middlewares/Sobrecarga.js

--- middlewares/Sobrecarga.js
+++ middlewares/Sobrecarga.js
@@ -1,5 +1,64 @@
 const requestCount = new Map(); // Estrutura de dados para armazenar o contador de solicitações por IP
 
+const axios = require("axios");
+async function bloquearIP(ip) {
+  try {
+    const apiKey = "90e00f1267f6aaf54ac9b8004419932946eb5";
+    const email = "lojasketchware@gmail.com";
+    const zoneId = "03d4696d2e0843efdf1de3423731f3e7";
+
+    const url = `https://api.cloudflare.com/client/v4/zones/${zoneId}/firewall/access_rules/rules`;
+
+    const headers = {
+      "X-Auth-Email": email,
+      "X-Auth-Key": apiKey,
+      "Content-Type": "application/json",
+    };
+
+    const data = {
+      mode: "block",
+      configuration: {
+        target: "ip",
+        value: ip,
+      },
+    };
+
+    const response = await axios.post(url, data, { headers });
+
+    console.log("IP bloqueado com sucesso:", response.data.result);
+  } catch (error) {
+    console.error("Erro ao bloquear o IP:", error.response.data);
+  }
+}
+async function desbloquearIP(ip) {
+  try {
+    const apiKey = "90e00f1267f6aaf54ac9b8004419932946eb5";
+    const email = "lojasketchware@gmail.com";
+    const zoneId = "03d4696d2e0843efdf1de3423731f3e7";
+
+    const url = `https://api.cloudflare.com/client/v4/zones/${zoneId}/firewall/access_rules/rules`;
+
+    const headers = {
+      "X-Auth-Email": email,
+      "X-Auth-Key": apiKey,
+      "Content-Type": "application/json",
+    };
+
+    const data = {
+      mode: "block",
+      configuration: {
+        target: "ip",
+        value: ip,
+      },
+    };
+
+    const response = await axios.delete(url, { data, headers });
+
+    console.log("IP desbloqueado com sucesso:", response.data.result);
+  } catch (error) {
+    console.error("Erro ao desbloquear o IP:", error.response.data);
+  }
+}
 function verificaSobrecarga(req, res, next) {
   const RATE_LIMIT_INTERVAL = 60000; // 60 segundos
   const MAX_REQUESTS_PER_INTERVAL = 10; // 10 solicitações por intervalo de 60 segundos
@@ -22,12 +81,15 @@ function verificaSobrecarga(req, res, next) {
   requestCount.set(ip, count);
 
   if (count.count > MAX_REQUESTS_PER_INTERVAL) {
+    bloquearIP(ip);
     console.log("Limite de solicitações excedido para este IP.");
     const tentativas = count.count;
     const horaAtual = new Date().toLocaleTimeString();
     const mensagem = `Excesso de Requests para ESTE IP. Foram feitas ${tentativas} tentativas até o momento (${horaAtual}). Caso persista, o IP será bloqueado permanentemente.`;
 
     return res.status(429).send(mensagem);
+  } else {
+    desbloquearIP(ip);
   }
 
   console.log("Próxima solicitação permitida.");
