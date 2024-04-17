c35dee41ed407fb0b348307481d220d25f4ccb62
viniciusdev772 <viniciusdev37@gmail.com>
2024-04-16 22:20:41 -0300

    added opti to get

--- routes/ApisRoutes.js
+++ routes/ApisRoutes.js
@@ -6,6 +6,11 @@ router.get("/", APIController.dashboard);
 router.post("/create", APIController.APICriar);
 router.post("/json", APIController.dashboardJson);
 
+router.get("/myip", async (req, res) => {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  res.send(ip);
+});
+
 router.post("/byjwt", APIController.byjwt);
 
 module.exports = router;
